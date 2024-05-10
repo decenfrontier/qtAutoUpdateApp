4779e865f54d88861576e3680177be08fb64e95f
decenfrontier <decenfrontier@gmail.com>
2024-05-10 09:34:04 +0800

    fix

--- run_write_version.py
+++ run_write_version.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
 import os
+import sys
 
-# version读取环境变量, 然后写入到version.py中的version变量中
-version = os.environ.get('version')
-print("version:", version)
+# 从命令行参数中读取版本号
+version = sys.argv[1]
 
 cur_file_path = os.path.dirname(__file__)
 versionFilePath = os.path.join(cur_file_path, "version.py")
-print("edit file {versionFilePath} output: version = {version}")
+print(f"edit file {versionFilePath} output: version = {version}")
 # 覆盖写出文件 version.py 中
 with open(versionFilePath, 'w') as f:
     f.write(f'version = "{version}"')
-
